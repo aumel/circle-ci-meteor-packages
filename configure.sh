193fb987df7519a96961f4768d49fbd7edb8d425
aumel <aurelienmelot@gmail.com>
2016-07-02 22:16:13 +0200

    Fix configuration

--- configure.sh
+++ configure.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 #configuring the system
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/Makefile
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/start_test.js
-wget https://raw.github.com/arunoda/travis-ci-meteor-packages/master/phantom_runner.js
+wget https://raw.githubusercontent.com/aumel/circle-ci-meteor-packages/master/start_test.js
+wget https://raw.githubusercontent.com/aumel/circle-ci-meteor-packages/master/phantom_runner.js
 
 #install meteor
 curl https://install.meteor.com | /bin/sh
-
-#installing meteorite
-npm install -g meteorite
