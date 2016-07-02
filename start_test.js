318fbb6eb6b2a27cc116ea883d921852d1d6a9ea
aumel <aurelienmelot@gmail.com>
2016-07-02 22:32:24 +0200

    Fix path to phantom.js

--- start_test.js
+++ start_test.js
@@ -38,7 +38,7 @@ meteor.stdout.on('data', function startTesting(data) {
 
 function runTestSuite() {
   process.env.URL = "http://localhost:10015/";
-  var phantomjs = spawn('phantomjs', ['./phantom_runner.js']);
+  var phantomjs = spawn('phantomjs', ['/home/ubuntu/phantom_runner.js']);
   phantomjs.stdout.pipe(process.stdout);
   phantomjs.stderr.pipe(process.stderr);
 
